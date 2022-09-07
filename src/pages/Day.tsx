64c430ec081223ceb5b8158e69dd50793fe2fc5d
yellowk4 <yellowk4@hunet.co.kr>
2022-09-07 18:06:06 +0900

    test

--- src/pages/Day.tsx
+++ src/pages/Day.tsx
@@ -30,7 +30,7 @@ export default function Day() {
 
 	return <div>
 		<h2>Day {day}</h2>
-		{words.length === 0 && <span>Loading...</span>}
+		{words.length === 0 && <span>Loading......</span>}
 		<table>
 			<tbody>
 				{words.map(word => (
